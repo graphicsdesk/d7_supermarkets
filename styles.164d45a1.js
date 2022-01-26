d7419b5869a553a62c82444fe2f0b6e19141a475
apark2020 <park.jayoon.andrew@gmail.com>
2022-01-26 01:52:56 -0500

    Deploy to gh-pages

--- styles.164d45a1.js
+++ styles.164d45a1.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42641" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42025" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
